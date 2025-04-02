6ba877aacdae64d464e2106164a92fbdf70aba42
SCSnowden06 <SCSnowden06@gmail.com>
2025-04-02 11:37:56 -0400

    ch5l1 success

--- FreeBootDev/main.py
+++ FreeBootDev/main.py
@@ -1,61 +1,18 @@
 class Human:
-    def sprint_right(self):
-        if self.__stamina <= 0:
-            self.__raise_if_cannot_sprint()
-        elif self.__stamina > 0:
-            self.__use_sprint_stamina()
-            self.move_right()
-            self.move_right()
+    def __init__(self, name):
+        self.__name = name
 
-    def sprint_left(self):
-        if self.__stamina <= 0:
-            self.__raise_if_cannot_sprint()
-        elif self.__stamina > 0:
-            self.__use_sprint_stamina()
-            self.move_left()
-            self.move_left()
+    def get_name(self):
+        return self.__name
 
-    def sprint_up(self):
-        if self.__stamina <= 0:
-            self.__raise_if_cannot_sprint()
-        elif self.__stamina > 0:
-            self.__use_sprint_stamina()
-            self.move_up()
-            self.move_up()
 
-    def sprint_down(self):
-        if self.__stamina <= 0:
-            self.__raise_if_cannot_sprint()
-        elif self.__stamina > 0:
-            self.__use_sprint_stamina()
-            self.move_down()
-            self.move_down()
+## don't touch above this line
 
-    def __raise_if_cannot_sprint(self):
-        raise Exception("not enough stamina to sprint")
 
-    def __use_sprint_stamina(self):
-        self.__stamina -= 1
+class Archer(Human):
+    def __init__(self, name, num_arrows):
+        super().__init__(name)
+        self.__num_arrows = num_arrows
 
-    # don't touch below this line
-
-    def move_right(self):
-        self.__pos_x += self.__speed
-
-    def move_left(self):
-        self.__pos_x -= self.__speed
-
-    def move_up(self):
-        self.__pos_y += self.__speed
-
-    def move_down(self):
-        self.__pos_y -= self.__speed
-
-    def get_position(self):
-        return self.__pos_x, self.__pos_y
-
-    def __init__(self, pos_x, pos_y, speed, stamina):
-        self.__pos_x = pos_x
-        self.__pos_y = pos_y
-        self.__speed = speed
-        self.__stamina = stamina
+    def get_num_arrows(self):
+        return self.__num_arrows
--- FreeBootDev/main_test.py
+++ FreeBootDev/main_test.py
@@ -1,78 +1,82 @@
 from main import *
 
 run_cases = [
-    ((0, 0, 5, 3), ["sprint_right"], (10, 0, None)),
-    (
-        (0, 0, 20, 3),
-        [
-            "sprint_left",
-            "sprint_left",
-            "sprint_left",
-        ],
-        (-120, 0, None),
-    ),
-    (
-        (1, 1, 3, 1),
-        ["sprint_down", "sprint_right"],
-        (1, -5, "not enough stamina to sprint"),
-    ),
+    ("Faramir", "Human", None),
+    ("Bard", "Archer", 1),
 ]
-
 
 submit_cases = run_cases + [
-    (
-        (1, 1, 5, 2),
-        ["sprint_left", "sprint_up", "sprint_down"],
-        (-9, 11, "not enough stamina to sprint"),
-    ),
+    ("Boromir", "Human", None),
+    ("Aragorn", "Human", None),
+    ("Legolas", "Archer", 93828),
 ]
 
 
-def test(human_args, methods, expected_output):
+def test_inheritance():
     print("---------------------------------")
-    print(f"Starting values:")
-    human = Human(*human_args)
-    print(f" * x: {human_args[0]}")
-    print(f" * y: {human_args[1]}")
-    print(f" * speed: {human_args[2]}")
-    print(f" * stamina: {human_args[3]}")
-    for method in methods:
-        print(f" - calling {method}...")
+    print("Inheritance Test:")
+    if "Archer" not in globals():
+        print("Archer class not found")
+        return False
+    if "Human" not in globals():
+        print("Human class not found")
+        return False
+    if not issubclass(Archer, Human):
+        print("Archer is not a child class of Human")
+        return False
+    print("Archer is a child class of Human")
+    return True
+
+
+def test(name, type, num_arrows):
+    print("---------------------------------")
+    print(f"Type:   {type}")
+    print(f"Name:   {name}")
+    if type == "Archer":
+        print(f"Arrows: {num_arrows}")
+    print("")
     try:
-        for method in methods:
-            getattr(human, method)()
-        actual_x, actual_y = human.get_position()
-        actual_err = None
+        if type == "Human":
+            human = Human(name)
+            print(f"Expecting name: {name}")
+            print(f"Actual name:    {human.get_name()}")
+            if human.get_name() == name:
+                return True
+            else:
+                return False
+        else:
+            archer = Archer(name, num_arrows)
+            print(f"Expecting name:   {name}")
+            print(f"Actual name:      {archer.get_name()}")
+            print(f"Expecting arrows: {num_arrows}")
+            print(f"Actual arrows:    {archer.get_num_arrows()}")
+            if archer.get_name() == name and archer.get_num_arrows() == num_arrows:
+                return True
+            else:
+                return False
     except Exception as e:
-        actual_x, actual_y = human.get_position()
-        actual_err = str(e)
-    expected_x, expected_y, expected_error = expected_output
-    print(f"Expected x: {expected_x}")
-    print(f"Actual   x: {actual_x}")
-    print(f"Expected y: {expected_y}")
-    print(f"Actual   y: {actual_y}")
-    print(f"Expected error: {expected_error}")
-    print(f"Actual   error: {actual_err}")
-    if (
-        actual_x == expected_x
-        and actual_y == expected_y
-        and actual_err == expected_error
-    ):
-        print("Pass")
-        return True
-    print("Fail")
-    return False
+        print(f"Error: {e}")
+        return False
 
 
 def main():
     passed = 0
     failed = 0
     skipped = len(submit_cases) - len(test_cases)
+    correct = test_inheritance()
+    if correct:
+        print("Pass")
+        passed += 1
+    else:
+        print("Fail")
+        failed += 1
     for test_case in test_cases:
         correct = test(*test_case)
         if correct:
+            print("Pass")
             passed += 1
         else:
+            print("Fail")
             failed += 1
     if failed == 0:
         print("============= PASS ==============")
